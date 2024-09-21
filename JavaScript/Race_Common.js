d4a73028ef1e4223a90e1fb5ca894e3975647eb2
nankotsu-Akh <ak1324az@docomo.ne.jp>
2024-09-21 18:47:56 +0900

    Update Race_Common.js

--- JavaScript/Race_Common.js
+++ JavaScript/Race_Common.js
@@ -12,6 +12,10 @@ const img_Frame = [
     "Img/Frame_Num_Color/40x40/8.png",
 ];
 
+/*================================================*/
+/* 関数定義定義 */
+/*================================================*/
+// 枠番画像のパスを取得
 function GetFrameMapImg_RelativePath(rel){
     return [
         rel + img_Frame[0],
@@ -23,4 +27,30 @@ function GetFrameMapImg_RelativePath(rel){
         rel + img_Frame[6],
         rel + img_Frame[7],
     ];
+}
+
+// メンバーの前走をDBから取得
+function SetBeforeRuns(MemberDB) {
+    MatchingRace.forEach(Races => {
+        Races[1].forEach(Mem => {
+            let id = Mem.MemberID;
+
+            // IDが登録されている場合に前走を挿入(モブは未登録)
+            if(id > 0 && id != undefined) {
+                // DBに登録されているデータの前走情報を4つまで登録
+                const res = MemberDB[id-1];
+                for(let i = 0; i < 4; i++){
+                    if(i < res.result.length) {
+                        // レース日以降のものは登録しない(前走なので)
+                        const nowRaceDay = Races[2].raceDay.year*10000 + Races[2].raceDay.month*100 + Races[2].raceDay.day;
+                        const befRaceDay = res.result[i].Date.year*10000 + res.result[i].Date.month*100 + res.result[i].Date.day;
+                        if( nowRaceDay > befRaceDay) {
+                            Mem.beforeRuns.push(res.result[i]);
+                        }
+                    }
+                    else break;
+                }
+            }
+        })
+    });
 }
