5bfa988bd705856322a614aedfb3c81d9143be2b
nankotsu-Akh <ak1324az@docomo.ne.jp>
2024-09-21 18:48:45 +0900

    Update Race_Contents1.js

--- JavaScript/Contents/Race_Contents1.js
+++ JavaScript/Contents/Race_Contents1.js
@@ -4,75 +4,75 @@ const MatchingRace = [
         [16,17,6,7,9,11,15,18,13,10,2,12,4,5,14,3,8,1],
         [
             {
-                "Frame":1, "Name":"ヤエノムテキ", "Strategy":"", "Runk":["-", ], "Trainer":"ぐましぃ", "TrainerID":"-", "GoalTime":"2:14.3", "GoalTimeDef":"7", "CornerPass":[8,8,8,12], "FurlongTime":"-", "Favorite":11, "parents":["-", "-"],
+                "Frame":1, "Name":"ヤエノムテキ", "Strategy":"", "Runk":["-", ], "MemberID":3, "GoalTime":"2:14.3", "GoalTimeDef":"7", "CornerPass":[8,8,8,12], "FurlongTime":"-", "Favorite":11, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":1, "Name":"アグネスタキオン", "Strategy":"先行", "Runk":["-", ], "Trainer":"なんこつ", "TrainerID":"-", "GoalTime":"2:12.2", "GoalTimeDef":"5/4", "CornerPass":[11,10,9,11], "FurlongTime":"-", "Favorite":12, "parents":["-", "-"],
+                "Frame":1, "Name":"アグネスタキオン", "Strategy":"先行", "Runk":["-", ], "MemberID":12, "GoalTime":"2:12.2", "GoalTimeDef":"5/4", "CornerPass":[11,10,9,11], "FurlongTime":"-", "Favorite":12, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":2, "Name":"マチカネフクキタル", "Strategy":"差し", "Runk":["-", ], "Trainer":"ヴァニラ", "TrainerID":"-", "GoalTime":"2:13.0", "GoalTimeDef":"3", "CornerPass":[15,15,17,17], "FurlongTime":"-", "Favorite":18, "parents":["-", "-"],
+                "Frame":2, "Name":"マチカネフクキタル", "Strategy":"差し", "Runk":["-", ], "MemberID":1, "GoalTime":"2:13.0", "GoalTimeDef":"3", "CornerPass":[15,15,17,17], "FurlongTime":"-", "Favorite":18, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":2, "Name":"アグネスデジタル", "Strategy":"差し", "Runk":["-", ], "Trainer":"春はあげぽよ", "TrainerID":"-", "GoalTime":"2:12.3", "GoalTimeDef":"5/4", "CornerPass":[17,17,18,18], "FurlongTime":"-", "Favorite":17, "parents":["-", "-"],
+                "Frame":2, "Name":"アグネスデジタル", "Strategy":"差し", "Runk":["-", ], "MemberID":24, "GoalTime":"2:12.3", "GoalTimeDef":"5/4", "CornerPass":[17,17,18,18], "FurlongTime":"-", "Favorite":17, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":3, "Name":"ミスターシービー", "Strategy":"追込", "Runk":["-", ], "Trainer":"REON", "TrainerID":"-", "GoalTime":"2:12.3", "GoalTimeDef":"クビ", "CornerPass":[16,16,15,14], "FurlongTime":"-", "Favorite":4, "parents":["-", "-"],
+                "Frame":3, "Name":"ミスターシービー", "Strategy":"追込", "Runk":["-", ], "MemberID":5, "GoalTime":"2:12.3", "GoalTimeDef":"クビ", "CornerPass":[16,16,15,14], "FurlongTime":"-", "Favorite":4, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":3, "Name":"サイレンススズカ", "Strategy":"逃げ", "Runk":["-", ], "Trainer":"takkun", "TrainerID":"-", "GoalTime":"2:11.2", "GoalTimeDef":"7/4", "CornerPass":[1,1,1,1], "FurlongTime":"-", "Favorite":3, "parents":["-", "-"],
+                "Frame":3, "Name":"サイレンススズカ", "Strategy":"逃げ", "Runk":["-", ], "MemberID":19, "GoalTime":"2:11.2", "GoalTimeDef":"7/4", "CornerPass":[1,1,1,1], "FurlongTime":"-", "Favorite":3, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":4, "Name":"タニノギムレット", "Strategy":"追込", "Runk":["-", ], "Trainer":"閻魔", "TrainerID":"-", "GoalTime":"2:11.2", "GoalTimeDef":"ｱﾀﾏ", "CornerPass":[14,14,14,15], "FurlongTime":"-", "Favorite":16, "parents":["-", "-"],
+                "Frame":4, "Name":"タニノギムレット", "Strategy":"追込", "Runk":["-", ], "MemberID":14, "GoalTime":"2:11.2", "GoalTimeDef":"ｱﾀﾏ", "CornerPass":[14,14,14,15], "FurlongTime":"-", "Favorite":16, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":4, "Name":"コパノリッキー", "Strategy":"逃げ", "Runk":["-", ], "Trainer":"とあ", "TrainerID":"-", "GoalTime":"2:13.2", "GoalTimeDef":"5/4", "CornerPass":[3,3,2,4], "FurlongTime":"-", "Favorite":7, "parents":["-", "-"],
+                "Frame":4, "Name":"コパノリッキー", "Strategy":"逃げ", "Runk":["-", ], "MemberID":11, "GoalTime":"2:13.2", "GoalTimeDef":"5/4", "CornerPass":[3,3,2,4], "FurlongTime":"-", "Favorite":7, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":5, "Name":"タイキシャトル", "Strategy":"先行", "Runk":["-", ], "Trainer":"みやび", "TrainerID":"-", "GoalTime":"2:11.2", "GoalTimeDef":"クビ", "CornerPass":[4,4,5,7], "FurlongTime":"-", "Favorite":1, "parents":["-", "-"],
+                "Frame":5, "Name":"タイキシャトル", "Strategy":"先行", "Runk":["-", ], "MemberID":6, "GoalTime":"2:11.2", "GoalTimeDef":"クビ", "CornerPass":[4,4,5,7], "FurlongTime":"-", "Favorite":1, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":5, "Name":"メジロパーマー", "Strategy":"差し", "Runk":["-", ], "Trainer":"ハイダラ", "TrainerID":"-", "GoalTime":"2:12.0", "GoalTimeDef":"3/4", "CornerPass":[2,2,4,9], "FurlongTime":"-", "Favorite":14, "parents":["-", "-"],
+                "Frame":5, "Name":"メジロパーマー", "Strategy":"差し", "Runk":["-", ], "MemberID":7, "GoalTime":"2:12.0", "GoalTimeDef":"3/4", "CornerPass":[2,2,4,9], "FurlongTime":"-", "Favorite":14, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":6, "Name":"ナリタタイシン", "Strategy":"追込", "Runk":["-", ], "Trainer":"ノーデ", "TrainerID":"-", "GoalTime":"2:11.2", "GoalTimeDef":"ｱﾀﾏ", "CornerPass":[13,13,12,6], "FurlongTime":"-", "Favorite":10, "parents":["-", "-"],
+                "Frame":6, "Name":"ナリタタイシン", "Strategy":"追込", "Runk":["-", ], "MemberID":16, "GoalTime":"2:11.2", "GoalTimeDef":"ｱﾀﾏ", "CornerPass":[13,13,12,6], "FurlongTime":"-", "Favorite":10, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":6, "Name":"ファインモーション", "Strategy":"先行", "Runk":["-", ], "Trainer":"うしうし", "TrainerID":"-", "GoalTime":"2:12.2", "GoalTimeDef":"ハナ", "CornerPass":[3,9,11,10], "FurlongTime":"-", "Favorite":13, "parents":["-", "-"],
+                "Frame":6, "Name":"ファインモーション", "Strategy":"先行", "Runk":["-", ], "MemberID":17, "GoalTime":"2:12.2", "GoalTimeDef":"ハナ", "CornerPass":[3,9,11,10], "FurlongTime":"-", "Favorite":13, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":7, "Name":"ワンダーアキュート", "Strategy":"先行", "Runk":["-", ], "Trainer":"そらお", "TrainerID":"-", "GoalTime":"2:11.9", "GoalTimeDef":"7/4", "CornerPass":[12,12,13,13,], "FurlongTime":"-", "Favorite":8, "parents":["-", "-"],
+                "Frame":7, "Name":"ワンダーアキュート", "Strategy":"先行", "Runk":["-", ], "MemberID":15, "GoalTime":"2:11.9", "GoalTimeDef":"7/4", "CornerPass":[12,12,13,13,], "FurlongTime":"-", "Favorite":8, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":7, "Name":"キングヘイロー", "Strategy":"差し", "Runk":["-", ], "Trainer":"ミカ", "TrainerID":"-", "GoalTime":"2:12.5", "GoalTimeDef":"3/2", "CornerPass":[5,5,7,5], "FurlongTime":"-", "Favorite":15, "parents":["-", "-"],
+                "Frame":7, "Name":"キングヘイロー", "Strategy":"差し", "Runk":["-", ], "MemberID":13, "GoalTime":"2:12.5", "GoalTimeDef":"3/2", "CornerPass":[5,5,7,5], "FurlongTime":"-", "Favorite":15, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":7, "Name":"アドマイヤベガ", "Strategy":"追込", "Runk":["-", ], "Trainer":"れじすた", "TrainerID":"-", "GoalTime":"2:11.4", "GoalTimeDef":"1", "CornerPass":[18,18,16,16], "FurlongTime":"-", "Favorite":6, "parents":["-", "-"],
+                "Frame":7, "Name":"アドマイヤベガ", "Strategy":"追込", "Runk":["-", ], "MemberID":4, "GoalTime":"2:11.4", "GoalTimeDef":"1", "CornerPass":[18,18,16,16], "FurlongTime":"-", "Favorite":6, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":8, "Name":"テイエムオペラオー", "Strategy":"先行", "Runk":["-", ], "Trainer":"むにえる", "TrainerID":"-", "GoalTime":"2:10.5", "GoalTimeDef":"-", "CornerPass":[7,6,3,3], "FurlongTime":"-", "Favorite":2, "parents":["-", "-"],
+                "Frame":8, "Name":"テイエムオペラオー", "Strategy":"先行", "Runk":["-", ], "MemberID":2, "GoalTime":"2:10.5", "GoalTimeDef":"-", "CornerPass":[7,6,3,3], "FurlongTime":"-", "Favorite":2, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":8, "Name":"アグネスタキオン", "Strategy":"先行", "Runk":["-", ], "Trainer":"きりゆたんぽ", "TrainerID":"-", "GoalTime":"2:10.9", "GoalTimeDef":"3/2", "CornerPass":[9,7,6,2], "FurlongTime":"-", "Favorite":9, "parents":["-", "-"],
+                "Frame":8, "Name":"アグネスタキオン", "Strategy":"先行", "Runk":["-", ], "MemberID":27, "GoalTime":"2:10.9", "GoalTimeDef":"3/2", "CornerPass":[9,7,6,2], "FurlongTime":"-", "Favorite":9, "parents":["-", "-"],
                 "beforeRuns":[]
             },
             {
-                "Frame":8, "Name":"ダイタクヘリオス", "Strategy":"逃げ", "Runk":["-", ], "Trainer":"かつせん", "TrainerID":"-", "GoalTime":"2:11.6", "GoalTimeDef":"3/2", "CornerPass":[10,11,10,8], "FurlongTime":"-", "Favorite":5, "parents":["-", "-"],
+                "Frame":8, "Name":"ダイタクヘリオス", "Strategy":"逃げ", "Runk":["-", ], "MemberID":8, "GoalTime":"2:11.6", "GoalTimeDef":"3/2", "CornerPass":[10,11,10,8], "FurlongTime":"-", "Favorite":5, "parents":["-", "-"],
                 "beforeRuns":[]
             },
         ],
