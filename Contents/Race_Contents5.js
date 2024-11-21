fb3337382e871383b6e63961ca100c35c7cfa162
nankotsu-Akh <ak1324az@docomo.ne.jp>
2024-11-21 23:08:26 +0900

    Add files via upload

---
+++ Contents/Race_Contents5.js
@@ -0,0 +1,276 @@
+const RaceGroupeID = 5;
+const MatchingRace = [
+    // レース出走情報　レース順
+    [   // 第1レース
+        [5,6,2,9,7,8,4,1,3],
+        [1044,11,1045,1046,6,1,105,115,107]
+    ],
+    [   // 第2レース
+        [5,7,4,1,8,2,6,9,3],
+        [12,117,1047,22,21,1048,116,103,1049]
+    ],
+    [   // 第3レース
+        [1,5,2,7,3,9,8,4,6],
+        [4,113,101,1050,108,1051,5,1052,19],
+        [
+            {
+                "Name":"アストンマーチャン", "Strategy":"逃げ",  "Runk":["UE4", 31469], "MemberID":4, "GoalTime":"1:07.1", "GoalTimeDef":"-", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":3, "parents":["-", "-"],
+                "beforeRuns":[]
+            },
+            {
+                "Name":"アストンマーチャン", "Strategy":"逃げ",  "Runk":["UE6", 32263], "MemberID":"", "Trainer":"やくも", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"アタマ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":4, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ヒシアケボノ", "Strategy":"先行",  "Runk":["UE8", 33288], "MemberID":"", "Trainer":"鈴八", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"5", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":5, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"オグレッセ", "Strategy":"逃げ",  "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":7, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"バンブーメモリー", "Strategy":"差し", "Runk":["UE8", 33733], "MemberID":"", "Trainer":"みざくら", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":6, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ジェバト", "Strategy":"差し", "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":9, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"カツラギエース", "Strategy":"逃げ", "Runk":["UE6", 32503], "MemberID":5, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":2, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"クラリネットリズム", "Strategy":"",  "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":8, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"サイレンススズカ", "Strategy":"逃げ", "Runk":["UE8", 33370], "MemberID":19, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":1, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+        ],
+        {
+            "raceTitle":"スプリンターズステークス",
+            "raceDay":{"year":24,"month":2, "day":12, "date":"月曜"},
+            "startTime":"-:-",
+            "season":"秋",
+            "timeZone":"昼",
+            "weather":"晴",
+            "field":"芝",
+            "condition":"良",
+            "course":{
+                "length":12,
+                "place":"中山",
+                "rotate":"右・外",
+            }
+        }
+    ],
+    [   // 第4レース
+        [5,7,3,2,1,8,4,6,9],
+        [119,16,28,1001,27,1001,112,114,1001],
+        [
+            {
+                "Name":"ナリタトップロード", "Strategy":"先行",  "Runk":["UE8", 33752], "MemberID":"", "Trainer":"たけざぁ", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"7", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":4, "parents":["-", "-"],
+                "beforeRuns":[]
+            },
+            {
+                "Name":"ナリタタイシン", "Strategy":"追込",  "Runk":["UE8", 33499], "MemberID":16, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":2, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ライスシャワー", "Strategy":"先行",  "Runk":["UE9", 34016], "MemberID":28, "GoalTime":"-", "GoalTimeDef":"8", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":1, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ベータキュビズム", "Strategy":"追込",  "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":9, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"サクラローレル", "Strategy":"差し", "Runk":["UE9", 34116], "MemberID":27, "TrainerID":0, "GoalTime":"3:13.0", "GoalTimeDef":"-", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":3, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ジュエルサファイア", "Strategy":"先行", "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":7, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"メジロブライト", "Strategy":"差し", "Runk":["UE9", 33930], "MemberID":"", "Trainer":"時津風", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":5, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"キタサンブラック", "Strategy":"逃げ", "Runk":["UE2", 30167], "MemberID":"", "Trainer":"omeeeeelet", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"6", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":6, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"リボンパストラル", "Strategy":"逃げ", "Runk":["A+", "-"], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"-", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":8, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+        ],
+        {
+            "raceTitle":"天皇賞・春",
+            "raceDay":{"year":24,"month":2, "day":12, "date":"月曜"},
+            "startTime":"-:-",
+            "season":"春",
+            "timeZone":"昼",
+            "weather":"晴",
+            "field":"芝",
+            "condition":"良",
+            "course":{
+                "length":32,
+                "place":"京都",
+                "rotate":"右・外",
+            }
+        }
+    ],
+    [   // 第5  レース
+        [7,9,1,3,5,6,2,4,8],
+        [2,1001,120,1001,106,17,111,1001,8],
+        [
+            {
+                "Name":"エアグルーヴ", "Strategy":"先行",  "Runk":["UD1", 35547], "MemberID":2, "GoalTime":"-", "GoalTimeDef":"アタマ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":4, "parents":["-", "-"],
+                "beforeRuns":[]
+            },
+            {
+                "Name":"ミニラベンダー", "Strategy":"逃げ", "Runk":["S", ], "MemberID":"", "Trainer":"", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":9, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"フジキセキ", "Strategy":"先行",  "Runk":["UE7", 33013], "MemberID":"", "Trainer":"チョラン", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"クビ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":6, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"マジャールロンド", "Strategy":"差し",  "Runk":["S", ], "MemberID":"", "Trainer":"-", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":7, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイイチルビー", "Strategy":"差し", "Runk":["UE5", 31943], "MemberID":"", "Trainer":"からす", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"3/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":5, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ファインモーション", "Strategy":"先行", "Runk":["UD3", 36761], "MemberID":17, "GoalTime":"-", "GoalTimeDef":"4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":2, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ヴィブロス", "Strategy":"差し", "Runk":["UD", ], "MemberID":"", "Trainer":"あたし", "TrainerID":0, "GoalTime":"1:30.2", "GoalTimeDef":"-", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":3, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"バイパーピアース", "Strategy":"差し", "Runk":["S", ], "MemberID":"", "Trainer":"", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":8, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイタクヘリオス", "Strategy":"逃げ", "Runk":["UD3", 36255], "MemberID":8, "GoalTime":"-", "GoalTimeDef":"5/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":1, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+        ],
+        {
+            "raceTitle":"朝日杯フューチュリティステークス",
+            "raceDay":{"year":24,"month":2, "day":12, "date":"月曜"},
+            "startTime":"-:-",
+            "season":"冬",
+            "timeZone":"昼",
+            "weather":"晴",
+            "field":"芝",
+            "condition":"良",
+            "course":{
+                "length":16,
+                "place":"阪神",
+                "rotate":"右・外",
+            }
+        }
+    ],
+    [   // 第6  レース
+        [11,6,8,9,10,17,18,7,15,16,4,12,1,14,3,5,2,13],
+        [103,121,120,21,20,113,23,8,19,117,11,14,106,17,6,15,115,107],
+        [
+            {
+                "Name":"オグリキャップ", "Strategy":"追込",  "Runk":["", ], "MemberID":"", "Trainer":"れおん", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"3/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":13, "parents":["-", "-"],
+                "beforeRuns":[]
+            },
+            {
+                "Name":"ヤエノムテキ", "Strategy":"先行", "Runk":["", ], "MemberID":"", "Trainer":"まるまる", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"3/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":3, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ユキノビジン", "Strategy":"先行",  "Runk":["", ], "MemberID":"", "Trainer":"チョラン", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"クビ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":17, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"バンブーメモリー", "Strategy":"差し",  "Runk":["", ], "MemberID":21, "GoalTime":"-", "GoalTimeDef":"2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":11, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ツインターボ", "Strategy":"先行", "Runk":["", ], "MemberID":20, "GoalTime":"-", "GoalTimeDef":"1", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":18, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイイチルビー", "Strategy":"差し", "Runk":["", ], "MemberID":"", "Trainer":"やくも", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"7/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":6, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"マルゼンスキー", "Strategy":"逃げ", "Runk":["", ], "MemberID":23, "GoalTime":"-", "GoalTimeDef":"7/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":12, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイタクヘリオス", "Strategy":"逃げ", "Runk":["", ], "MemberID":8, "GoalTime":"-", "GoalTimeDef":"3/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":4, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ラインクラフト", "Strategy":"先行", "Runk":["", ], "MemberID":19, "GoalTime":"-", "GoalTimeDef":"アタマ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":7, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイイチルビー", "Strategy":"差し", "Runk":["", ], "MemberID":"", "Trainer":"でんげん", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"クビ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":9, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ラインクラフト", "Strategy":"先行", "Runk":["", ], "MemberID":11, "GoalTime":"1:31.5", "GoalTimeDef":"-", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":8, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"タニノギムレット", "Strategy":"追込", "Runk":["", ], "MemberID":14, "GoalTime":"-", "GoalTimeDef":"アタマ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":16, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ダイイチルビー", "Strategy":"差し", "Runk":["", ], "MemberID":"", "Trainer":"からす", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":15, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ファインモーション", "Strategy":"先行", "Runk":["", ], "MemberID":17, "GoalTime":"-", "GoalTimeDef":"ハナ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":1, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"タイキシャトル", "Strategy":"先行", "Runk":["", ], "MemberID":6, "GoalTime":"-", "GoalTimeDef":"7/4", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":2, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ワンダーアキュート", "Strategy":"先行", "Runk":["", ], "MemberID":15, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":14, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"シーキングザパール", "Strategy":"先行", "Runk":["", ], "MemberID":"", "Trainer":"ボンレス兄貴", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"1/2", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":10, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+            {
+                "Name":"ケイエスミラクル", "Strategy":"先行", "Runk":["", ], "MemberID":"", "Trainer":"はご", "TrainerID":0, "GoalTime":"-", "GoalTimeDef":"クビ", "CornerPass":[0], "FurlongTime":"00.0", "Favorite":5, "parents":["-", "-"],
+                "beforeRuns":[],
+            },
+        ],
+        {
+            "raceTitle":"阪神ジュベナイルフィリーズ",
+            "raceDay":{"year":24,"month":2, "day":12, "date":"月曜"},
+            "startTime":"-:-",
+            "season":"冬",
+            "timeZone":"昼",
+            "weather":"晴",
+            "field":"芝",
+            "condition":"良",
+            "course":{
+                "length":16,
+                "place":"阪神",
+                "rotate":"右・外",
+            }
+        }
+    ],
+]
