a1c834b4e706e8711feb7ce880e68ebab1d9e13b
nankotsu-Akh <ak1324az@docomo.ne.jp>
2025-02-11 00:52:34 +0900

    Add files via upload

--- JavaScript/Contents/Race_Contents8.js
+++ JavaScript/Contents/Race_Contents8.js
@@ -1,28 +1,34 @@
 const RaceGroupeID = 8;
 const MatchingRace = [
     [
-        [],
-        []
+        [12,6,3,1,7,10,8,11,5,2,9,4],
+        [ENUM_RINRIN.BonressBro, ENUM_RINRIN.Disyupa, ENUM_Members.takkun, 1154, ENUM_RINRIN.Leon, ENUM_RINRIN.Atashi,
+         ENUM_RINRIN.Marumaru, ENUM_RINRIN.Mizakura, 1155, ENUM_RINRIN.Goro, ENUM_Members.Umashib, ENUM_Members.Nankotsu]
     ],
     [
-        [],
-        []
+        [11,10,3,12,8,9,4,5,7,2,6,1],
+        [1156, ENUM_RINRIN.Seshiru, ENUM_RINRIN.Yakumo, ENUM_Members.Umashib, ENUM_Members.Katsusen, 1054,
+         ENUM_RINRIN.Dengen, ENUM_RINRIN.BonressBro, ENUM_Members.BroMan, ENUM_Members.Sorao, ENUM_Members.Resiter, ENUM_RINRIN.Atashi]
     ],
     [
-        [],
-        []
+        [8,6,11,5,10,7,9,3,4,12,2,1],
+        [1157, 1158, ENUM_RINRIN.Yakumo, ENUM_RINRIN.Leon, ENUM_RINRIN.Dengen, ENUM_Members.Vanilla,
+         ENUM_RINRIN.Seshiru, ENUM_RINRIN.Marumaru, ENUM_Members.takkun, ENUM_Members.REON, ENUM_Members.Katsusen, ENUM_Members.Ushiushi]
     ],
     [
-        [],
-        []
+        [11,7,3,1,9,6,5,2,8,12,10,4],
+        [ENUM_Members.REON, ENUM_Members.Sorao, ENUM_Members.Ushiushi, 1159, ENUM_RINRIN.Goro, ENUM_Members._108,
+         ENUM_Members.BroMan, ENUM_RINRIN.Mizakura, ENUM_Members.Vanilla, 1160, ENUM_Members.Resiter, ENUM_Members.Disyupa]
     ],
     [
-        [],
-        []
+        [12,6,5,8,4,3,10,7,2,1,9,11],
+        [ENUM_Members.Sorao, ENUM_RINRIN.Goro, ENUM_RINRIN.Mizakura, ENUM_Members.BroMan, ENUM_Members.Umashib, ENUM_RINRIN.Marumaru,
+         ENUM_Members.Katsusen, ENUM_RINRIN.Disyupa, 1161, ENUM_RINRIN.BonressBro, 1162, ENUM_Members._108]
     ],
     [
-        [],
-        []
+        [4,2,6,11,10,7,8,12,5,9,1,3],
+        [1163, ENUM_Members.Ushiushi, 1164, ENUM_Members.Resiter, ENUM_Members.takkun, ENUM_Members.REON,
+         ENUM_RINRIN.Leon, ENUM_Members.Vanilla, ENUM_RINRIN.Seshiru, ENUM_RINRIN.Dengen, ENUM_RINRIN.Atashi, ENUM_RINRIN.Yakumo]
     ],
 ]
 
