cfa2a9c3c4f4f215acecba57379e1fe508cb57dd
nankotsu-Akh <ak1324az@docomo.ne.jp>
2025-08-18 13:49:52 +0900

    Create Race_Contents9.js

---
+++ JavaScript/Contents/Race_Contents9.js
@@ -0,0 +1,24 @@
+const RaceGroupeID = 9;
+const MatchingRace = [
+    [
+        [],
+        [ENUM_Members.Values.Munieru, ENUM_Members.Values.BroMan, ENUM_Members.Values.takkun,
+         Enum_Members.Values.Resiter, Enum_Members.Values.Sorao, Enum_Members.Values.Masa,
+         1001, 1002, 1003]
+    ],
+    [
+        [],
+        []
+    ],
+    [
+        [],
+        []
+    ],
+  [
+        [],
+        []
+    ],
+  [
+        [],
+        []
+    ],
